16edd471c7a92d41114d9ab3e32815554a7f42ad
EwenLM <ewenqglm@gmail.com>
2025-12-16 13:23:04 +0100

    fix config vite

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,14 @@ import { Home } from './pages/Home'
 import { Blog } from './pages/Blog'
 import { Portfolio } from './pages/Portfolio'
 
+const basePath = import.meta.env.BASE_URL
+
 export function App() {
   return (
     <Router>
-      <Home path="/" />
-      <Blog path="/blog" />
-      <Portfolio path="/portfolio" />
+      <Home path={`${basePath}`} />
+      <Blog path={`${basePath}blog`} />
+      <Portfolio path={`${basePath}portfolio`} />
     </Router>
   )
 }
